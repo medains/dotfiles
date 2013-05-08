b8c36e84ab6a5df945fead05194f6a6b5db68f0b
Medains <medains@github.com.fake>
2013-05-08 15:09:03 +0100

    Initial version of setup script

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 dotfiles
 ========
+
+A collection of configuration files that I can easily apply on new hardware.
+
+``` bash
+git clone https://github.com/medains/dotfiles.git
+cd dotfiles
+./setup.sh
+```
+
---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Not my original work, this script comes from:
+# https://github.com/TomNomNom/dotfiles/
+#
+# Thanks to Tom Hudson for this
+
+echo "Setting up this user with standard dotfiles"
+
+dotfilesDir=$(pwd)
+
+function linkDotfile {
+    dest="${HOME}/${1}"
+    dateStr=$(date +%Y-%m-%d-%H%M)
+    if [ -h ~/${1} ]; then
+        echo "Removing existing symlink: ${dest}"
+        rm ${dest}
+    elif [ -f "${dest}" ]; then
+        echo "Backing up existing file: ${dest}"
+        mv ${dest}{,.${dateStr}}
+    elif [ -d "${dest}" ]; then
+        echo "Backing up existing dir: ${dest}"
+        mv ${dest}{,.${dateStr}}
+    fi
+    echo "Creating new symlink: ${dest}"
+    ln -s ${dotfilesDir}/${1} ${dest}
+}
+
+linkDotfile .vim
+linkDotfile .vimrc
